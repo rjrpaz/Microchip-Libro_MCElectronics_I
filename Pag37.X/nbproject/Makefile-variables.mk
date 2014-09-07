6c77fd0025b9fe02d4a1411275cfb503d6b060e7
Roberto J. R. Paz <rjrpaz@gmail.com>
2014-09-06 22:54:52 -0300

    Agregado el ejemplo de la página 37, que utiliza la función "printf". En nuestro caso la reemplazamos por putsUSART, que es la intención del ejemplo.

---
+++ Pag37.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Pag37.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Pag37.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pag37.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pag37.x.tar
