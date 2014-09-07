e32bd35abc7ca5fa0b886d720a62084825737fe2
Roberto J. R. Paz <rjrpaz@gmail.com>
2014-09-07 03:34:26 -0300

    Ejemplo de página 45, utiliza "while". A medida que se presiona el switch, un led hace toggle.

---
+++ Pag45.X/main.c
@@ -0,0 +1,36 @@
+#pragma config FOSC = INTIO67, FCMEN = OFF, IESO = OFF                       // CONFIG1H
+#pragma config PWRT = OFF, BOREN = SBORDIS, BORV = 30                        // CONFIG2L
+#pragma config WDTEN = OFF, WDTPS = 32768                                     // CONFIG2H
+#pragma config MCLRE = OFF, LPT1OSC = OFF, PBADEN = ON, CCP2MX = PORTC       // CONFIG3H
+#pragma config STVREN = ON, LVP = OFF, XINST = OFF                          // CONFIG4L
+#pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF                   // CONFIG5L
+#pragma config CPB = OFF, CPD = OFF                                         // CONFIG5H
+#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF               // CONFIG6L
+#pragma config WRTB = OFF, WRTC = OFF, WRTD = OFF                           // CONFIG6H
+#pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF           // CONFIG7L
+#pragma config EBTRB = OFF                                                  // CONFIG7H
+
+
+#include <xc.h>
+#include <plib/delays.h>
+
+void main (void)
+{
+    TRISD = 0b01111111;     	// PORTD bits 7:0 are all outputs (0)
+    INTCON2bits.RBPU = 0;	// enable PORTB internal pullups
+    WPUBbits.WPUB0 = 1;		// enable pull up on RB0
+    ANSELH = 0x00;              // AN8-12 are digital inputs (AN12 on RB0)
+    TRISBbits.TRISB0 = 1;       // PORTB bit 0 (connected to switch) is input (1)
+
+    while (1)
+    {
+        if (PORTBbits.RB0 == 1) {
+            LATDbits.LATD7 = !LATDbits.LATD7;
+            Delay10TCYx(25);
+
+            while (PORTBbits.RB0 == 1)
+                Delay10TCYx(25);
+        }
+    }
+	
+}
